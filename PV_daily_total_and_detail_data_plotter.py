8a81ef45d993cc7b1b2a214a3ece34b9c89145d2
tsherwen <ts551@york.ac.uk>
2014-02-09 13:47:41 +0000

    updated module

--- PV_daily_total_and_detail_data_plotter.py
+++ PV_daily_total_and_detail_data_plotter.py
@@ -2,12 +2,20 @@ import matplotlib.font_manager as font_manager
 import matplotlib.pyplot as plt
 from PV_read_tools import *
 
+# Where is the data?
 wd ='/Users/Tomas/Dropbox/Personal/Ele_and_Tomas_s_Shared_Folder/PV_panels_dat_files/'
 
+# Get data
+dates_I, min_15_kwh, titles, labels = daily_detail_read(wd)
 dates, day_kwh, labels, y_av, y_av_II, y_av_III , win_, win_II, win_III   = daily_total_read(wd)
+print titles , len(min_15_kwh), len(dates_I) , len(dates_I)/4/24
+print len(day_kwh),len(dates)
 
+# Plot data
 fig = plt.figure(figsize=(20,10), dpi=80, facecolor='w', edgecolor='k')
 ax1 = fig.add_subplot(111)
+
+plt.plot(dates_I, min_15_kwh, color='purple')
 plt.plot(dates, day_kwh, marker='x', color='b', alpha=0.3)
 plt.plot(dates[win_:-win_], y_av[win_:-win_], color='green', ls='dashed', label = '5 day moving avg.' )
 plt.plot(dates[win_II:-win_II], y_av_II[win_II:-win_II], color='orange' ,ls ='dashed' , label = '10 day moving avg.' )
@@ -16,7 +24,13 @@ plt.xticks(dates[2::31], labels[2::31])
 plt.xlim(dates[0]-1, dates[-1]+1 )
 plt.ylabel( 'Daily kWh output' )
 plt.legend(loc='upper right',prop=font_manager.FontProperties(size=20))
+
+# plot on second y
+ax2 = ax1.twinx()
+ax2.plot(dates_I, min_15_kwh, color='purple')
+
+
+
 plt.rcParams.update({'font.size': 20})
 plt.title('PV output, York (3.6 kW SunPower )')
-
 plt.show()
--- PV_read_tools.py
+++ PV_read_tools.py
@@ -1,24 +1,66 @@
 import numpy as np
 import matplotlib as mpl
 import datetime
+import glob
 import time
 
 def movingaverage(interval, window_size):
     window = np.ones(int(window_size))/float(window_size)
     return np.convolve(interval, window, 'same')
 
-def daily_total_read(reader):
+def daily_total_read(wd=None,reader=None):
+    if (reader == None ):
+        reader = open(wd+'getDailyTotals_all.csv', 'rb')
     data=[]
     for row in reader:       
-        print row
+#        print row
         data.append( np.array([ item.split(',') for item in row.splitlines()] )[0][:, np.newaxis] )
     data = np.concatenate(data, axis=1)
     titles, dates, day_kwh = data[:,0], data[0,1:] , data[1,1:]
-    labels = [ time.strftime('%b', i) for i in  [time.strptime(date, '%Y-%m-%d') for date in dates]  ]
-    dates = np.array( [mpl.dates.datestr2num(date) for date in dates] )#, np.float )
+    dates, labels  = time_2_num_and_labels(dates)
     day_kwh = np.array(day_kwh,    np.float )
     x,    y                  = dates, day_kwh
     win_, win_II, win_III    = 5,  10, 15   
     y_av, y_av_II, y_av_III  =  movingaverage(y, win_), movingaverage(y, win_II), movingaverage(y, win_III)
     return dates, day_kwh, labels, y_av, y_av_II, y_av_III , win_, win_II, win_III 
 
+def time_2_num_and_labels(dates):
+    try:
+        labels = [ time.strftime('%b', i) for i in  [time.strptime(date, '%Y-%m-%d') for date in dates]  ]
+    except:
+        labels = [ time.strftime('%b', i) for i in  [time.strptime(date, '%Y-%m-%d %H:%M') for date in dates]  ]
+
+    dates = np.array( [mpl.dates.datestr2num(date) for date in dates] )
+    return dates, labels 
+
+def remove_titles(data):
+    data_p = []
+    titles = data[:,0]
+    for i in range( len( data[0,:] ) ):
+        if data[0,i] == 'Date':
+            print data[:,i]
+        else :
+            data_p.append( data[:,i][:,np.newaxis] )
+#    print len( data ), data.shape, titles
+    data_p = np.concatenate( data_p, axis = 1 )
+    return data_p, titles
+
+def daily_detail_read(wd=None,reader=None):
+    if (reader == None):
+        files =  glob.glob(wd+'getDailyDetails_*')
+    data=[]                                                                                                                                              
+    for file in files:
+        reader = open(file)
+        print file
+        for row in reader:
+#            print row
+            data.append( np.array([ item.split(',') for item in row.splitlines()] )[0][:, np.newaxis] )    
+#    print len(data), len(data[0])
+    data = np.concatenate(data, axis=1)
+#    print data.shape
+    data, titles  = remove_titles(data)
+    dates = data[0,:]
+#    print dates
+    min_15_kwh = data[1,:] 
+    dates, labels = time_2_num_and_labels(dates)
+    return dates, min_15_kwh, titles, labels
