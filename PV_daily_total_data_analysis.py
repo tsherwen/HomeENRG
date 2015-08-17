19543d274c481ff4bc88cf57d7cad96b5dabd9c6
tsherwen <ts551@york.ac.uk>
2015-08-17 09:33:44 +0100

    update .gitignore + restore analysis func from prior to merge

--- PV_daily_total_data_analysis.py
+++ PV_daily_total_data_analysis.py
@@ -8,12 +8,17 @@ from scipy import fftpack
 # get data
 wd ='/Users/Tomas/Dropbox/Personal/Ele_and_Tomas_s_Shared_Folder/PV_panels_dat_files/'
 fn = 'getDailyTotals_all_II_14_03_09.csv'
-rm_last = True
-dates, day_kwh, labels, y_av, y_av_II, y_av_III , win_, win_II, win_III = daily_total_read(wd, fn, rm_last=rm_last)
+dates, day_kwh, labels, y_av, y_av_II, y_av_III , win_, win_II, win_III   = daily_total_read(wd, fn)
 
 # plot
 fig = plt.figure(figsize=(20,10), dpi=80, facecolor='w', edgecolor='k')
 ax1 = fig.add_subplot(111)
+#plt.plot(dates, day_kwh, marker='x', color='b', alpha=0.3)
+#plt.plot(dates[win_:-win_], y_av[win_:-win_], color='green', ls='dashed', label = '5 day moving avg.' )
+#plt.plot(dates[win_II:-win_II], y_av_II[win_II:-win_II], color='orange' ,ls ='dashed' , label = '10 day moving avg.' )
+#plt.plot(dates[win_III:-win_III], y_av_III[win_III:-win_III], color='r' ,ls ='dashed' , label = '15 day moving avg.' )
+#plt.xticks(dates[2::31], labels[2::31])
+#plt.xlim(dates[0]-1, dates[-1]+1 )
 
 curve_fit = False # add sinododal fit
 if (curve_fit):
